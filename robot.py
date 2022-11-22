d7848558b9f9ef35b21159afc1fe5cfda0a7285a
GrassyAirplane <euanlimcs@gmail.com>
2022-11-22 16:02:36 +0800

    Robot addition inventory

--- robot.py
+++ robot.py
@@ -5,5 +5,11 @@ Implementation of robot that moves the packages
 """
 
 class Robot:
+    robot_character = "R"
+
     def __init__(self, current_tile: tile.Tile):
+        # Sets to R by default
+        self.character = Robot.robot_character
+        self.inventory = None # shoud contain package
         pass
+
